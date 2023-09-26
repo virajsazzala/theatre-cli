b6f0294bd5a2da57f5d0aa3becf44b01e45bf20c
SNG Viraj Reddy <vir200319@gmail.com>
2023-09-26 23:56:11 +0530

    Update setup.py with author info

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='theatre-cli',
-    version='1.0.0',
+    version='0.1.0',
     description='A command-line interface to play movies',
-    long_description=open('README.md').read(),
+    long_description=open('README.rst').read(),
     long_description_content_type='text/markdown',
-    author='Your Name',
-    author_email='your.email@example.com',
-    url='https://github.com/yourusername/theatre-cli',
+    author='SNG Viraj Reddy',
+    author_email='vir200319@gmail.com',
+    url='https://github.com/virajsazzala/theatre-cli',
     license='MIT',
     packages=find_packages(),
     install_requires=[
