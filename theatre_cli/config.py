4694d2f5e99946d76439859700b523ce1589f498
lexcsam <mishraviswash9@gmail.com>
2023-10-06 06:40:53 +0530

    moved config.json to .config dir to comply with linux norms

--- theatre_cli/config.py
+++ theatre_cli/config.py
@@ -1,6 +1,8 @@
 import json
-
-CONFIG_FILE = "config.json"
+import os
+#Changing the target path to the new folder that we created accodring to linux folder structure conventions
+CONFIG_DIR = os.path.expanduser("~/.config/theatre-cli")
+CONFIG_FILE = os.path.join(CONFIG_DIR, "config.json")
 
 def load_config():
     try:
@@ -10,5 +12,6 @@ def load_config():
         return []
 
 def save_config(config_data):
+    os.makedirs(CONFIG_DIR, exist_ok=True)
     with open(CONFIG_FILE, "w") as f:
         json.dump(config_data, f, indent=2)
