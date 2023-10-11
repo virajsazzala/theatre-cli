f347aefcdc826503c9253b78e0af513fc9630180
anniepauline <anniepauline.23@gmail.com>
2023-10-11 17:38:24 +0530

    updates congif.py

--- theatre_cli/config.py
+++ theatre_cli/config.py
@@ -1,14 +1,16 @@
 import yaml
 
-CONFIG_FILE = "config.json"
+CONFIG_FILE = "config.yaml"
+
 
 def load_config():
     try:
         with open(CONFIG_FILE, "r") as yf:
             # fullLoader converts yaml to py dict
-            return yaml.load(yf,Loader=yaml.FullLoader)
+            return yaml.load(yf, Loader=yaml.FullLoader)
     except FileNotFoundError:
         return []
+
 
 def save_config(config_data):
     with open(CONFIG_FILE, "w") as yf:
